4d378a1604e1c300214411f54efac2fa22be1ec3
Paula G Aldazabal <72109193+paugaldazabal@users.noreply.github.com>
2021-04-18 16:05:26 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,17 @@
 function cargarProductos() {
 	for (const producto of productos) {
 		$('.col-md-4').append(`<div>
-							   <img src="${producto.img}" alt="${producto.alt}" width="33%" height="auto" id="${producto.id}">
-							   <div class="desc">
-							   <span style="font-size:24px"><strong>${producto.nombre}</strong></span>
-							   <br>
-							   <span style="font-size:20px">$${producto.precio}</span>
-							   <br>
-							   <i>${producto.características}</i>
-							   <br>
-							   <button class="agregar-al-carrito" onclick="respuestaClickAgregar(${producto.id})" id=${producto.id}>¡LO QUIERO!</button>
-							   </div>
-							   </div>`);
+					<img src="${producto.img}" alt="${producto.alt}" width="33%" height="auto" id="${producto.id}">
+					<div class="desc">
+					<span style="font-size:24px"><strong>${producto.nombre}</strong></span>
+					<br>
+					<span style="font-size:20px">$${producto.precio}</span>
+					<br>
+					<i>${producto.características}</i>
+					<br>
+					<button class="agregar-al-carrito" onclick="respuestaClickAgregar(${producto.id})" id=${producto.id}>¡LO QUIERO!</button>
+					</div>
+					</div>`);
 	}
 }
 
@@ -113,33 +113,33 @@ function respuestaClickSelecciónComprar(e) {
 // RENDER CARRITO (SALIDA DEL CARRITO SEGUN LA INFORMACION DEL ARRAY DE SELECCIONADOS)
 function generarCarritoProductos() {
 	$('#resumen-productos').html(`<table id="tabla-carrito">
-								  <thead>
-							  	  <tr>
-								  <th>PRODUCTO</th>
-								  <th>PRECIO</th>
-								  <th>CANTIDAD</th>
-								  <th>SUBTOTAL</th>
-								  <th></th>
-								  </tr>
-								  </thead>
-								  <tbody id="table-body-carrito"></tbody>
-								  </table>`);
+					<thead>
+					<tr>
+					<th>PRODUCTO</th>
+					<th>PRECIO</th>
+					<th>CANTIDAD</th>
+					<th>SUBTOTAL</th>
+					<th></th>
+					</tr>
+					</thead>
+					<tbody id="table-body-carrito"></tbody>
+					</table>`);
 	
 	let iconoEliminarProducto = `<svg id="trash" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-    							 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-    							 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-  								 </svg>`;
+    					<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+    					<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+  					</svg>`;
 
   	let total = 0;
 
   	for (const producto of SELECCIONADOS) {
   		$('#table-body-carrito').append(`<tr id="productos-listados-tabla">
-  										 <td>${producto.nombre}</td>
-						      		     <td id="precios">$${producto.precio}</td>
-						      	 	     <td id="unidades">${producto.unidad}</td>
-						      			 <td>$${producto.obtenerSubtotal()}</td>
-						      			 <td><span id="eliminar-de-carrito" onclick="eliminar(${producto.id})">${iconoEliminarProducto}</span></td>
-						   				 </tr>`);
+  						    <td>${producto.nombre}</td>
+						    <td id="precios">$${producto.precio}</td>
+						    <td id="unidades">${producto.unidad}</td>
+						    <td>$${producto.obtenerSubtotal()}</td>
+						    <td><span id="eliminar-de-carrito" onclick="eliminar(${producto.id})">${iconoEliminarProducto}</span></td>
+						    </tr>`);
   		// TOTAL COMPRA
 		// OBTENER LA SUMA DE precioFinal ENTRE TODOS LOS OBJETOS DEL ARRAY
 		total = SELECCIONADOS.reduce(function (previos, actual) {
@@ -159,34 +159,34 @@ function generarCarritoProductos() {
 
 	// FORMULARIO FINALIZAR COMPRA: (ARREGLAR EL TOTAL!)
 	$('#finalizar-compra-formulario').html(`<div class="content__paragraph4">
-											<h1 class="heading">FINALIZAR COMPRA</h1>
-											<div class="contacto-box">
-											<div class="contacto-box__productos-contacto">
-											<hr>
-											<p id="total-compra"><strong>Total compra: $ ${total}</strong></p>
-											</div>
-											<div class="form-style">
-											<form action="" name="formulario-finalizar-compra" id="formulario-finalizar-compra" onsubmit="respuestaFinalizarCompra(event)"> 
-											<input type="text" id="nombre" placeholder="Nombre"><br> 
-											<input type="email" id="email" placeholder="Email"><br>
-											<input type="tel" id="teléfono" placeholder="Teléfono"><br> 
-											<label for="ubicacion">Cantidad de cuotas</label><br>
-											<select id="cuotas-elegidas" name="ubicacion">
-											<option value="1">1  Pagos de $ ${total}</option>
-											<option value="2">3  Pagos de $ ${totalTresCuotas}</option>
-											<option value="3">6  Pagos de $ ${totalSeisCuotas}</option>	
-											</select><br>
-											<label for="tarjeta">Medio de pago</label><br>
-											<input type="tel" id="tarjeta" placeholder="Número de tarjeta"><br>
-											<input type="text" id="titular" placeholder="Titular de tarjeta"><br> 
-											<input type="tel" id="vencimiento" placeholder="Vencimiento (MM/AA)"><br>
-											<input type="tel" id="cvv" placeholder="CVV"><br>
-											<input type="submit" value="FINALIZAR COMPRA">
-											</form>
-											<p id="mensaje-error-2" style="display: none">Por favor, completá todos los campos para finalizar la compra.</p>
-											</div>
-											</div>
-										    </div>`);
+						<h1 class="heading">FINALIZAR COMPRA</h1>
+						<div class="contacto-box">
+						<div class="contacto-box__productos-contacto">
+						<hr>
+						<p id="total-compra"><strong>Total compra: $ ${total}</strong></p>
+						</div>
+						<div class="form-style">
+						<form action="" name="formulario-finalizar-compra" id="formulario-finalizar-compra" onsubmit="respuestaFinalizarCompra(event)"> 
+						<input type="text" id="nombre" placeholder="Nombre"><br> 
+						<input type="email" id="email" placeholder="Email"><br>
+						<input type="tel" id="teléfono" placeholder="Teléfono"><br> 
+						<label for="ubicacion">Cantidad de cuotas</label><br>
+						<select id="cuotas-elegidas" name="ubicacion">
+						<option value="1">1  Pagos de $ ${total}</option>
+						<option value="2">3  Pagos de $ ${totalTresCuotas}</option>
+						<option value="3">6  Pagos de $ ${totalSeisCuotas}</option>	
+						</select><br>
+						<label for="tarjeta">Medio de pago</label><br>
+						<input type="tel" id="tarjeta" placeholder="Número de tarjeta"><br>
+						<input type="text" id="titular" placeholder="Titular de tarjeta"><br> 
+						<input type="tel" id="vencimiento" placeholder="Vencimiento (MM/AA)"><br>
+						<input type="tel" id="cvv" placeholder="CVV"><br>
+						<input type="submit" value="FINALIZAR COMPRA">
+						</form>
+						<p id="mensaje-error-2" style="display: none">Por favor, completá todos los campos para finalizar la compra.</p>
+						</div>
+						</div>
+						</div>`);
 }
 
 
@@ -236,11 +236,11 @@ function respuestaFinalizarCompra(event) {
 		$('#mensaje-error-2').hide();
 
 		$('#mensaje-final').html(`<div id="mensaje-final-content">
-								  <span id="close" onclick="cerrarMensajeFinal()">&times;</span>
-								  <div>CONFIRMACIÓN DE PEDIDO</div>
-								  <div>¡Gracias por elegirnos, ${nombre}! Estaremos enviando tu pedido en las próximas 24 horas.<br>
-								  Por favor, revisá el correo que te enviamos a ${email}</div>
-								  </div>`);
+					  <span id="close" onclick="cerrarMensajeFinal()">&times;</span>
+					  <div>CONFIRMACIÓN DE PEDIDO</div>
+					  <div>¡Gracias por elegirnos, ${nombre}! Estaremos enviando tu pedido en las próximas 24 horas.<br>
+					  Por favor, revisá el correo que te enviamos a ${email}</div>
+					  </div>`);
 
 		$('#mensaje-final').fadeOut(0.5, function() {
 			$('#mensaje-final').fadeIn(2000);
